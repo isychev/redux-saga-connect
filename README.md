8ca5c4aa39155379b8eb83cbb7a88bb7bbf18f15
Ilya Sychev <ILYA.SYCHEV@raiffeisen.ru>
2019-02-01 19:28:39 +0300

    init project

---
+++ README.md
@@ -0,0 +1,110 @@
+redux-saga-connect is a decorator react components to connect the sagas runtime.
+### Example
+```javascript
+export default withSaga({mainSaga})(Component);
+```
+# Getting started
+
+## Install
+
+```sh
+$ npm install --save redux-saga-connect
+```
+or
+
+```sh
+$ yarn add redux-saga-connect
+```
+
+## Usage Example
+When you create a redux store function *sagaMiddleware.run* must be saved to the store object named *runSaga*
+```javascript 
+// create store
+// ...
+import createSagaMiddleware from 'redux-saga'
+// ...
+const sagaMiddleware = createSagaMiddleware()
+// ...
+store.runSaga = sagaMiddleware.run; // name `runSaga` required
+// ...
+```
+Decorator using React.Context gets access to the runSaga function
+In the component constructor (to launch saga as quickly as possible), sagas is started using the *runSaga* function. 
+In *componentWillUnmount* will cancel running sagas
+### Example
+```javascript
+// with es6
+import React from 'react';
+import withSaga from 'redux-saga-connect'
+// React component
+const Component = () => (<div>Component</div>);
+// Saga
+function* mainSaga(){};
+// Return enhancered component
+export default withSaga({mainSaga})(Component);
+// with recompose
+export default compose(withSaga({mainSaga}))(Component)
+```
+```typescript jsx
+// with typescript
+import React from 'react';
+import withSaga from 'redux-saga-connect'
+
+// React component
+interface ComponentProps {}
+const Component: React.FC<ComponentProps> = () => <div>Component</div>;
+// Saga
+function* mainSaga() {}
+// Return enhancered compoenent
+export default withSaga<ComponentProps>({ mainSaga })(Component);
+
+// with recompose
+export default compose<ComponentProps, ComponentProps>(withSaga({ mainSaga }))(
+  Component,
+);
+```
+## Parameters
+withSaga is a decorator with parameters
+
+withSaga(objectOfSagas)
+ objectOfSagas  {
+     [key:string]: Saga or Object
+ }
+ Object description
+| Name | Type  |Require | Default value  | Descripttion |
+| ------------- |:-------------:| -----:| -----:|-----:|
+| saga   | Function generator | true |  - | Saga functioin
+| force | Boolean  |  false  |  false | If a Saga with this name already exists, the current Saga will get a unique name in will start
+| hold | Boolean      |   false  | false | Saga will not delete in componentWillUnmount
+### Example parameters
+```javascript
+import withSaga from 'redux-saga-connect';
+
+const Component = () => (<div/>);
+function* firstSaga(){};
+function* secondSaga(){};
+
+export default withSaga({ firstSaga })(Component);
+
+// or 
+export default withSaga({
+  firstSaga,
+  secondSaga,
+})(Component);
+
+// or 
+export default withSaga({
+  nameFirstSaga: firstSaga,
+  nameSecondSaga: secondSaga,
+})(Component);
+
+// or 
+export default withSaga({
+  nameFirstSaga: firstSaga,
+  nameSecondSaga: {
+      saga: secondSaga,
+      hold: true,   //  saga will not delete in componentWillUnmount
+      force: true,  // saga will get a unique name in will start
+  },
+})(Component);
+```
---
+++ src/index.tsx
@@ -0,0 +1,159 @@
+import * as React from "react";
+
+type Saga0 = () => Iterator<any>;
+type Saga1 = (...arg1: any[]) => Iterator<any>;
+
+export type Saga = Saga0 | Saga1;
+
+export interface Task {
+  isRunning(): boolean;
+  cancel(): void;
+}
+
+export type SagaRun = <S extends Saga>(saga: S, ...args: any[]) => Task;
+
+export interface SagaParams {
+  force?: boolean;
+  hold?: boolean;
+}
+export interface SagaInputParams extends SagaParams {
+  saga: Saga;
+}
+
+export interface SagaComponent extends SagaInputParams {
+  name: string;
+}
+
+export interface StoreSaga extends SagaComponent {
+  count: number;
+  task?: Task;
+}
+
+interface SagaInputObj {
+  [key: string]: Saga | SagaInputParams;
+}
+
+export interface StoreWithSaga {
+  runSaga: SagaRun;
+  sagas: StoreSaga[];
+}
+
+interface Context {
+  store: StoreWithSaga | null;
+}
+
+type withSagaType = <P extends {}>(
+  newSagas: SagaInputObj,
+) => (Component: React.ComponentType<P>) => React.ComponentType<P>;
+
+export function instanceOfSagaParams(
+  saga: Saga | SagaInputParams,
+): saga is SagaInputParams {
+  return "saga" in saga;
+}
+
+export const getNormalizeSaga = (
+  sagaObj: Saga | SagaInputParams,
+): SagaInputParams => {
+  if (instanceOfSagaParams(sagaObj)) {
+    const { saga, force = false, hold = false } = sagaObj;
+    return { saga, force, hold };
+  }
+  return {
+    force: false,
+    hold: false,
+    saga: sagaObj,
+  };
+};
+
+const withSaga: withSagaType = (newSagas: SagaInputObj) => <P extends {}>(
+  Component: React.ComponentType<P>,
+): React.ComponentType<P> =>
+  class WithAppendSaga extends React.Component<P> {
+    public static contextTypes = {
+      store: () => null,
+    };
+
+    public ownSagas: SagaComponent[] = [];
+
+    public store: StoreWithSaga | null;
+
+    constructor(props: P, context: Context) {
+      super(props, context);
+      if (context && context.store && context.store.runSaga) {
+        this.store = context.store;
+        this.store.sagas = this.store.sagas || [];
+        const { runSaga, sagas: storeSagas } = this.store;
+        Object.keys(newSagas).forEach((sagaName: string) => {
+          const { saga, force, hold } = getNormalizeSaga(newSagas[sagaName]);
+          const finalSagaName = force
+            ? `${sagaName}_${Math.random() * 10000000}`
+            : sagaName;
+
+          this.ownSagas.push({
+            force,
+            hold,
+            name: finalSagaName,
+            saga,
+          });
+
+          const storeSaga = storeSagas.find(
+            (sagaObj: StoreSaga) => sagaObj.name === finalSagaName,
+          );
+
+          if (storeSaga && !force) {
+            storeSaga.count += 1;
+          } else {
+            storeSagas.push({
+              count: 1,
+              force,
+              hold,
+              name: finalSagaName,
+              saga,
+              task: runSaga(saga, props),
+            });
+          }
+        });
+      } else {
+        this.store = null;
+
+        if (process.env.NODE_ENV !== "production") {
+          console.error(
+            "RunSaga function not found. You need to add runSaga to the Store",
+          );
+        }
+      }
+    }
+
+    public componentWillUnmount() {
+      if (this.store && this.store.runSaga) {
+        const { sagas } = this.store;
+        this.ownSagas.forEach((ownSaga: SagaComponent) => {
+          const { name, hold } = ownSaga;
+          const storeSaga = sagas.find((saga: StoreSaga) => saga.name === name);
+          if (storeSaga) {
+            if (storeSaga.count > 1 || hold) {
+              storeSaga.count -= 1;
+            } else if (
+              storeSaga.count === 1 &&
+              storeSaga.task &&
+              storeSaga.task.isRunning()
+            ) {
+              storeSaga.task.cancel();
+              if (this.store) {
+                this.store.sagas = this.store.sagas.filter(
+                  (saga: StoreSaga) => saga.name !== storeSaga.name,
+                );
+              }
+            }
+          }
+        });
+      }
+    }
+
+    public render() {
+      return <Component {...this.props} />;
+    }
+  };
+
+export default withSaga;
